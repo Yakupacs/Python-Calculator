2f2438966fd61b77e700e54160f8930b41d0c143
Muhammed Ali Açış <ykpacs@gmail.com>
2022-04-01 02:25:14 +0300

    Color changed.

--- HesapMakinesi.py
+++ HesapMakinesi.py
@@ -1,3 +1,4 @@
+from cgitb import text
 from tkinter import *
 
 def yaz(x):
@@ -10,7 +11,7 @@ s1 = 0
 def islemler(x):
     global hesap
     hesap = x
-    global s1
+    global s1                         
     s1 = float(giris.get())
     giris.delete(0, 'end')
 
@@ -34,6 +35,8 @@ def hesapla():
 
 pencere = Tk()
 pencere.geometry("430x470")
+pencere.configure(background='#282925')
+
 
 giris = Entry(font = "Arial 14 bold", width=35, justify=RIGHT)
 giris.place(x = 20, y = 20)
@@ -41,19 +44,20 @@ giris.place(x = 20, y = 20)
 b = []
 
 for i in range(1,10):
-    b.append(Button(text=str(i),font="Arial 30 bold", width=3, command=lambda x=i:yaz(x)))
+    b.append(Button(text=str(i),font="Arial 30 bold",fg='white', width=3, command=lambda x=i:yaz(x)))
 
 sayac = 0
 
 for i in range(0,3):
     for j in range(0,3):
         b[sayac].place(x = 22 + j * 100, y = 60 + 100 * i)
+        b[sayac].configure(background='black')
         sayac += 1
 
 islem = []
 
 for i in range(0,4):
-    islem.append(Button(font='Arial 30 bold', width=3, command = lambda x = i : islemler(x)))
+    islem.append(Button(font='Arial 30 bold',fg='white', width=3, command = lambda x = i : islemler(x)))
 
 islem[0]['text'] = "+"
 islem[1]['text'] = "-" 
@@ -62,12 +66,16 @@ islem[3]['text'] = "/"
 
 for i in range(0,4):
     islem[i].place(x = 322, y= 60 + 100 * i)
+    islem[i].configure(background='#191A18')
 
 altSatir = []
 
-sb = Button(text = "0", font = "Arial 30 bold", width=3, command = lambda x=0:yaz(x))
-nb = Button(text = ".", font = "Arial 30 bold", width=3, command = lambda x=".":yaz(x))
-eb = Button(text = "=", font = "Arial 30 bold", width=3, command = hesapla)
+sb = Button(text = "0", font = "Arial 30 bold",fg='white', width=3, command = lambda x=0:yaz(x))
+nb = Button(text = ".", font = "Arial 30 bold",fg='white', width=3, command = lambda x=".":yaz(x))
+eb = Button(text = "=", font = "Arial 30 bold",fg='white', width=3, command = hesapla)
+eb.configure(background='#191A18')
+nb.configure(background='#191A18')
+sb.configure(background='#191A18')
 
 altSatir.append(sb)
 altSatir.append(nb)
